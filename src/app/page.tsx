d5faf3ca03a0aa616352b1a0b82d5a28f47875cf
joemmalatesta <joemmalatesta@gmail.com>
2024-12-04 23:22:50 -0500

    random

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,21 +20,35 @@ export default function Home() {
 
 	return (
 		<main className="min-h-screen p-8 flex items-center justify-center flex-col">
-			<h1 className="text-3xl font-bold mb-3 opacity-70">Changelog Generator</h1>
+			<h1 className="text-4xl font-bold mb-5 opacity-70">Changelog Generator</h1>
 			<form onSubmit={handleSubmit} className="max-w-md w-full">
+				<div className="flex w-full">
 				<input 
 					type="text" 
 					name="repo" 
-					placeholder="Enter GitHub repository URL" 
-					className="w-full p-2 border-2 border-b-0 rounded-t text-black focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 border-neutral-900/30" 
+					placeholder="https://github.com/owner/repository"
+					className="w-11/12 p-2 border-2 rounded-l border-r-0 text-black focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 border-indigo-500/60" 
 					required 
-				/>
+					/>
 				<button 
 					type="submit" 
-					className="px-4 py-2 bg-indigo-500 text-white rounded-b hover:bg-indigo-600 transition-colors w-full"
-				>
-					Submit
+					className="px-4 py-2 rounded-r flex justify-center items-center bg-indigo-500 text-white hover:bg-indigo-600 transition-colors"
+					>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none" 
+						stroke="currentColor"
+						strokeWidth="1.5"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					>
+						<path d="M5 12h14M12 5l7 7-7 7"/>
+					</svg>
 				</button>
+					</div>
 			</form>
 		</main>
 	);
